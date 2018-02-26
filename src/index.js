2fff18a33e4c56d223c6ea2b0ec01f005f037a87
Mggeroff <roman.mager@outlook.com>
2018-02-26 18:59:30 +0300

    Pass only 14 tests

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+
+    let arr = str.split('');
+
+    let count = 0;
+    // ' | ( | ) '
+    // [
+    //     ['(', ')'],
+    //     ['|', '|']
+    // ];
+    arr.forEach((bracket) => {
+        if (count < 0) {
+            return false;
+        }
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            // проверем на открывающуюся скобку
+            if (bracket === bracketsConfig[j][0]) {
+                count++
+            } else if (bracket === bracketsConfig[j][1]) {
+                count--
+            } else {
+                break;
+            }
+        }
+    });
+
+    return count === 0;
 }
